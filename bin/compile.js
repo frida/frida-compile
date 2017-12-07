83a7190bd65f66004f4cecc315dc5e176389f6a3
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-12-07 18:25:14 +0100

    Skip babelify when compiling TypeScript code

--- bin/compile.js
+++ bin/compile.js
@@ -33,6 +33,10 @@ const options = {
   useAbsolutePaths: !!program.useAbsolutePaths
 };
 
+if (inputPath.endsWith('.ts')) {
+  options.babelify = false;
+}
+
 if (!watch) {
   compiler.build(inputPath, outputPath, options)
   .catch(error => {
