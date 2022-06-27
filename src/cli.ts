a0a482580a827667ef58565f1d0bd4830621dee8
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-06-27 23:39:56 +0200

    Move to ESM and simplify internals (#45)
    Reimplementing bundling on top of the TypeScript compiler.

---
+++ src/cli.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+import * as compiler from "./compiler.js";
+
+async function main() {
+    program
+        .usage("[options] <module>")
+        .requiredOption("-o, --output <file>", "write output to <file>")
+        .option("-w, --watch", "watch for changes and recompile")
+        .option("-S, --no-source-maps", "omit source-maps")
+        .option("-c, --compress", "compress using terser");
+
+    program.parse();
+
+    const opts = program.opts();
+    const compilerOpts: compiler.Options = {
+        projectRoot: process.cwd(),
+        inputPath: program.args[0],
+        outputPath: opts.output,
+        sourceMaps: opts.sourceMaps ? "included" : "omitted",
+        compression: opts.compress ? "terser" : "none",
+    };
+
+    if (opts.watch) {
+        compiler.watch(compilerOpts);
+    } else {
+        await compiler.build(compilerOpts);
+    }
+}
+
+main()
+    .catch(e => {
+        console.error(e);
+        process.exitCode = 1;
+    });
